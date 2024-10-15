5966b137f77effce4abf372f460b794befd8bab0
Kurman11 <skedyd45@naver.com>
2024-10-15 21:40:03 +0900

    featur/core

--- src/pages/main/page.jsx
+++ src/pages/main/page.jsx
@@ -1,5 +1,19 @@
 import PageStyled from "./styled"; 
 
+const { Container } = PageStyled;
+
 export default function Main(){
-  return <div>메인입니다.</div>;
+  return(
+  <Container>
+    <div className="container">
+      <div className="main-area1">1</div>
+      <div className="main-area2">2</div>
+      <div className="main-area3">3</div>
+      <div className="main-area4">4</div>
+      <div className="main-area5">5</div>
+      <div className="main-area6">6</div>
+      <div className="main-area7">7</div>
+    </div>
+  </Container>
+  );
 }
--- src/pages/main/styled.jsx
+++ src/pages/main/styled.jsx
@@ -2,6 +2,13 @@ import styled from "styled-components";
 
 const PageStyled = {
   Container : styled.div`
+  .main-area1{
+    top: 105px;
+    left: 148px;
+    width: 1200px;
+    height: 764px;
+    border: 1px solid black
+  }
   `
 };
 
